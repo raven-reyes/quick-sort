b4ba04cf77b015918842a90bf5f3ac66af3caf7b
Faith Nyamai <92273493+raven-reyes@users.noreply.github.com>
2022-11-20 19:25:01 +0300

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -2,51 +2,47 @@ import java.util.Arrays;
 
 public class QuickSort {
 	
-	public static int  partition(int arr[] ,  int low,   int high) {
+	public static int  partition(int arr[] ,  int start,   int end) {
           
-		int pivot = arr[low]; // pivot
-		int  i = low;     // index of  first element
-        int  j = high;   // index of the last element
+		int pivot = arr[start]; // pivot
+		int  i = start;  // index of first element
+                int  j = end;   //  index of last element
         
-		while( i < j) {   // while index of i is less than index of j
+		while( i < j) {   // while i is less than  j
 			// and while current [first] element is smaller or equals to pivot and index of that element is less than array length,
 			while( arr[i] <= pivot && i < arr.length-1) {
-				i++;    // increment index of smaller element
+				i++;    // increment i
 			}  
 			// while current [last] element is greater than pivot and index of that element is greater or equals to 0,
 			 while(arr[j] > pivot && j >=0) {
 				 j--; // decrement j 
 			 }
-		
-		     // if index of i is less than index of j 
-			 // and if arr[i] is greater than pivot and arr[j] is less than pivot , then swap arr[i] with arr[j]
+		        // this following piece of code will exceute if the above two while loop conditions are false and while i < j
+		     // if i is less than j 
+		   // and if arr[i] is greater than pivot and arr[j] is less than pivot , then swap arr[i] with arr[j]
 		    if(i < j) { 
 			int temp = arr[i];
 			arr[i] = arr[j];
 			arr[j] = temp;
 		}
 	}   
-		// if index of i is greater or equals to index of j , then 
-		// swap pivot element with arr[j]
-		if(i >= j) {
-			int temp = arr[low];
-			arr[low] = arr[j];
+		// when i becomes greater than j then,swap pivot element with current arr[j]
+			int temp = arr[start];
+			arr[start] = arr[j];
 			arr[j] = temp;
-		
-		}
-		return j; // return index of pivot
+		        return j; // return index of pivot
 	}
 	
-	public static void quickSort(int arr[], int low, int high) {
-		// This method is only invoked when we're processing arrays that have more than one element, 
-		//hence the partitioning only takes place if low < high.
-		if(low < high) {
-			// The partition(arr, low, high) method partitions the array, 
-			//and upon it's execution the variable index stores the index of the pivot after the partitioning.
-			int index = partition(arr, low, high);
+	public static void RecursiveQuickSort(int arr[], int start, int end) {
+		//This method is only invoked when we're processing arrays that have more than one element, 
+		//hence the partitioning only takes place if start  <  end
+		if(start < end) {
+			//The partition(arr, start , end )partitions the array, 
+			//and upon it's execution the variable index stores the index of the pivot element returned by the partition method
+			int index = partition(arr, start,end);
 			//Separately sort elements before partition and after partition
-			quickSort(arr , low, index-1);
-			quickSort(arr, index +1 , high);
+			RecursiveQuickSort(arr,start,index-1);
+			RecursiveQuickSort(arr,index+1, end);
 			
 		}
 	}
@@ -55,8 +51,7 @@ public class QuickSort {
 		
 		int [] arr = {7,5, -4,9, 13, 3, 12,4}; // the array
 		System.out.println("the original array is : " + Arrays.toString(arr)); // printing the original array
-		quickSort(arr, 0, arr.length-1); //  sorting the whole array
-		
+		RecursiveQuickSort(arr,0,arr.length-1); //  sorting the whole array
 		System.out.println("the sorted array is ; " + Arrays.toString(arr)); // printing the sorted array
  
 	}
